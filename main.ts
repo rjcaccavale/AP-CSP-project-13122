0701ccfed64f5549e747279012ff377123400daa
Richard <91565778+rjcaccavale@users.noreply.github.com>
2022-02-10 14:38:12 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,27 @@
-function yesNo1 () {
+function redo () {
+    game.splash("Please input a proper response (ex: \"yes\", \"no\")")
+    game.reset()
+}
+function answerSets () {
     if (answer == "yes") {
         game.splash("You're good to go, then!")
         game.over(true, effects.confetti)
     } else if (answer == "no") {
-        game.splash("Would you like to create a savings plan?")
-        step += 1
-        if (true) {
-        	
+        answer = game.askForString("Would you like to create a savings plan?")
+        if (answer == "no") {
+            game.splash("You should think about your financial future.")
+            game.over(false, effects.dissolve)
+        } else if (answer == "yes") {
+            game.splash("test")
+        } else {
+            redo()
         }
     } else {
-        game.splash("Please input a proper response (ex: \"yes\", \"no\")")
-        game.reset()
+        redo()
     }
 }
 let answer = ""
 let yesNO = ["yes", "no"]
 game.splash("Do you have 5-7 months worth of money in a secure, liquid place?")
 answer = game.askForString("For this program, please respond in lowercase.")
-let step = 0
-yesNo1()
+answerSets()
